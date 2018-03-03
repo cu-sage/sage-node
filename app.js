ad5387d9bb270835343d67d5c18d55e0d251161d
alexdziena <alexdziena@gmail.com>
2018-03-03 00:14:17 -0500

    Adding the connection to mongodb if the server is running under IIS

--- app.js
+++ app.js
@@ -18,8 +18,18 @@ var app = express();
 module.exports = require('./config/express')(app, config);
 
 /* If we are running in IIS, we did not start via "node ./bin/www".
-Therefore, start listening here. */ 
+Therefore, start listening here. */
 if (process.env.hasOwnProperty("IISNODE_VERSION")) {
+  var http = require('http');
+  mongoose.connect(config.db);
+  var db = mongoose.connection;
+
+  db.once('open', function(){
+    console.log('Connected to ' + config.db);
+  });
+  db.on('error', function () {
+    throw new Error('unable to connect to database at ' + config.db);
+  });
   app.listen(config.port, function () {
     console.log('Express server listening on port ' + config.port);
   });
